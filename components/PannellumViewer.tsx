825f80cd323e80341ae2738b0f2518312a49cd17
CentrovisonThailand <CentrovisonThailand@gmail.com>
2026-04-28 10:05:24 +0700

    Initial commit for Triple Room

--- components/PannellumViewer.tsx
+++ components/PannellumViewer.tsx
@@ -26,7 +26,7 @@ export default function PannellumViewer() {
     script.onload = () => {
       window.pannellum.viewer('panorama-container', {
         type: 'equirectangular',
-        panorama: '/image/Executive Twin Final.jpg',
+        panorama: '/image/Triple Room Final.jpg',
         autoLoad: true,
         autoRotate: -2,
       });
